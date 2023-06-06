fa6e20000a90844d337c9ad6f7b0d17b61c778f2
Gagana30 <gagana.ganu3024@gmail.com>
2023-06-06 15:02:23 +0530

    api
    api

---
+++ api.js
@@ -0,0 +1,66 @@
+let express = require('express');
+let app = express();
+let port = process.env.PORT||1995;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+let {dbconnection,getdata,postData} = require('./controller/dbconnect');
+
+
+// middlewear
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('hii from express to gagana')
+})
+
+app.get('/mainPage',async (req,res)=>{
+    let query = {}
+    let collection = "mainPage";
+    let output = await getdata(collection,query);
+    res.send(output);
+})
+
+app.get('/product',async (req,res)=>{
+    let query = {};
+    let collection = "product";
+    let output = await getdata(collection,query);
+    res.send(output);
+})
+
+app.get('/ordering',async (req,res)=>{
+    let query = {};
+    let collection = "ordering";
+    let output = await getdata(collection,query);
+    res.send(output);
+})
+
+app.get('/orderedDetails',async (req,res)=>{
+    let query = {}
+    if(req.query.Shoes_Id){
+        query={Shoes_Id: Number(req.query.Shoes_Id)}
+    }else if(req.query.BestSellers_Id){
+        query={BestSellers_Id: Number(req.query.BestSellers_Id)}
+    }else{
+        query = {}
+    }
+    let collection = "orderedDetails";
+    let output = await getdata(collection,query);
+    res.send(output);
+})
+
+// postData
+app.post('/orderedDetails',async (req,res)=> {
+    let data = req.body;
+    let collection = "orderedDetails";
+    let response = await postData(collection,data);
+    res.send(response);
+})
+
+app.listen(port,(err,data)=>{
+    dbconnection()
+    if(err) throw err;
+    console.log('server is runing on port ${port}')
+})
+
---
+++ controller/dbconnect.js
@@ -0,0 +1,46 @@
+let mongo = require('mongodb');
+let {MongoClient} = require('mongodb');
+// let mongoUrl = "mongodb://127.0.0.1:27017";
+
+let mongoUrl = "mongodb+srv://gagana:2QeTGJRqQ2iNZa95@allbirdsdb.jywio13.mongodb.net/?retryWrites=true&w=majority";
+let client = new MongoClient(mongoUrl)
+
+async function dbconnection(){
+    await client.connect();
+}
+
+let db = client.db('allBirds');
+
+async function getdata(colname,query){
+    let output = [];
+    try{
+        const cursor = db.collection(colname).find(query);
+        for await(const data of cursor){
+            output.push(data);
+        }
+        cursor.closed;
+    } catch(err){
+        output.push({"Error":"Error in getdata"})
+    }
+    return output
+}
+
+
+// postData
+async function postData(colname,data){
+    let output;
+    try{
+        output = await db.collection(colname).insertOne(data);
+    }catch(err){
+        output = {"response":"Error in postData"};
+    }
+    return output;
+}
+
+module.exports = {
+    dbconnection,
+    getdata,
+    postData
+}
+
+
