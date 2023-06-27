92c274261968626800633e37f61d3463ea27d420
Gana <127880189+Gagana30@users.noreply.github.com>
2023-06-27 11:49:07 +0530

    Update api.js

--- api.js
+++ api.js
@@ -25,7 +25,7 @@ app.get('/mainPage',async (req,res)=>{
 app.get('/product',async (req,res)=>{
     let query = {};
     if(req.query.Everyday_Id && req.query.Everyday){
-        query={Everyday_Id: Number(req.query.Everyday_Id),"Everyday_Id": Number(req.query.Everyday)}
+        query={"Everyday_Id": Number(req.query.Everyday_Id),"Everyday_Id": Number(req.query.Everyday)}
     }else if(req.query.Active_Id){
         query={Active_Id: Number(req.query.Active_Id)}
     }else if(req.query.Tree_Id){
